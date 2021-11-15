7bda34fa8b35e81bb61ce0a843db2b364eda4cbe
MerveDemirden <74743028+MerveDemirden@users.noreply.github.com>
2021-11-16 00:03:48 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ let dolarBugun = 9.30
 
 let dolarDun = 9.20
 dolarDun = "Ankara"
+//  Javascript dili rahatlıkla tipi değiştirebilir. Çıktı da dolarDun Ankara sonucunu verir...
 {
     let dolarDun = 9.10
 }
@@ -12,22 +13,27 @@ dolarDun = "Ankara"
 console.log(dolarDun)
 
 const euroDun = 11.2
-//euroDun = 11 // const sabit olduğundan bu satırı eklersek hata verir. 
+//euroDun = 11 
+//  "const" veri tiplerinden biridir. Anlamı sabit demektir. Yukarıdakinin sonucunda 'read only' hatası gelir. 
+//Sebebi ise sabite başka atama yapılamamasındandır...
+
 
 console.log(euroDun)
 
 //array (dizi)
-//camelCasing 
-//PascalCasing
-let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
+//camelCasing; değişken tanımlarken kullanılan bir notasyondur...
+//PascalCasing; başharf büyük
+//<li>; liste elemanı 
+
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi", "Özel Konut Kredisi]
 
 console.log("<ul>")
 for (let i = 0; i<konutKredileri.length;i++) {
+    
  console.log("<li>"+konutKredileri[i]+"</li>")
+    
 }
 console.log("</ul>")
-
-
 
 
 console.log(konutKredileri)
